ec5bb72cc32969a59c59aca74b4b9bb3b8c3b4bd
RafaMateos <rafaelmateostorres@gmail.com>
2018-10-31 14:37:17 +0100

    Hacer para el lunes

---
+++ 06-ConexBD/06-ConexBD/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _06_ConexBD.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index() {
+
+            return View();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost,ActionName ("Index")]
+        public ActionResult IndexPost()
+        {
+            SqlConnection miConexion = new SqlConnection();
+
+            try
+            {
+
+                miConexion.ConnectionString = "server=serverpersona.database.windows.net;database=personasDB;uid=Prueba44;pwd=123qwerty;";
+
+                miConexion.Open();
+
+                ViewData["Estado"] = miConexion.State;
+
+            }
+            catch (SqlException)
+            {
+
+                ViewData["Estado"] = "Error al intentar conectarse a la BD";
+
+            }
+
+            finally
+            {
+
+                miConexion.Close();
+
+            }
+
+            return View();
+        }
+
+        public ActionResult ListadoPersonas() {
+
+            //Aqui va todo el ej, pero desp conectaremos con otra clase, esto es una chapuza pa aprende
+
+            return View();
+        }
+
+    }
+}
---
+++ 06-ConexBD/06-ConexBD/Models/clsPersona.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _06_ConexBD.Models
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class clsPersona
+    {
+        #region Constructor PD
+        public clsPersona() {
+            //Por defecto
+        }
+        #endregion
+
+        #region Constructor PP
+        public clsPersona(int IdDept, int idPersona,string nombre,string apellidos,DateTime fechaNacimiento,string telefono,string direccion ) {
+
+            this.idPersona = idPersona;
+            this.nombre = nombre;
+            this.Apellidos = apellidos;
+            this.fechaNacimiento = fechaNacimiento;
+            this.telefono = telefono;
+            this.direccion = direccion;
+            this.IdDept = IdDept;
+
+        }
+
+        #endregion
+        #region atributos y propiedades
+        public int idPersona { get; set; }
+        public String nombre { get; set; }
+        public String Apellidos { get; set; }
+        public DateTime fechaNacimiento { get; set; }
+        public String direccion { get; set; }
+        public String telefono { get; set; }
+        public int IdDept { get; set; }
+
+
+        #endregion
+    }
+}
